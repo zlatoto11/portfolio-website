ffdf57254233b5b848418a3692ddec9450a32496
Zlatomir Kosev <zlatomir_hk@hotmail.com>
2020-09-07 18:35:21 +0100

    Updates

---
+++ precache-manifest.8f0c2f99093912acae63e96cb00b133c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2e0ca88e2f6faac921efd69aed4470",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "f3bb94c33485609da575",
+    "url": "/portfolio-website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e5cf6aacdc89aeb7f491",
+    "url": "/portfolio-website/static/css/main.6f2bed80.chunk.css"
+  },
+  {
+    "revision": "f3bb94c33485609da575",
+    "url": "/portfolio-website/static/js/2.9256bf7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-website/static/js/2.9256bf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5cf6aacdc89aeb7f491",
+    "url": "/portfolio-website/static/js/main.4af4ed61.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  },
+  {
+    "revision": "f1e089ca063cb769e4746dc0f3531efe",
+    "url": "/portfolio-website/static/media/Dissertation.f1e089ca.pdf"
+  },
+  {
+    "revision": "22cbe9af8b2b49a49291fcf967dbc27a",
+    "url": "/portfolio-website/static/media/GamesReport.22cbe9af.pdf"
+  },
+  {
+    "revision": "4fee49b1fad6abf0d1ef0439d93b67da",
+    "url": "/portfolio-website/static/media/IdeationCardsImage.4fee49b1.png"
+  },
+  {
+    "revision": "378f22506c8d6958a287dcff15f2fcb1",
+    "url": "/portfolio-website/static/media/RunningApplication.378f2250.pdf"
+  },
+  {
+    "revision": "239722ebc22d25e2a4bbf9721c72f641",
+    "url": "/portfolio-website/static/media/RunningTrackerApplicationImage.239722eb.jpg"
+  },
+  {
+    "revision": "22a02f0161e693ca1949f4328a3aca05",
+    "url": "/portfolio-website/static/media/SDLLogo.22a02f01.png"
+  },
+  {
+    "revision": "d8f2babb3e4e3073e8bdd13dce3fa471",
+    "url": "/portfolio-website/static/media/ThirdYearDissertation.d8f2babb.pdf"
+  },
+  {
+    "revision": "d92ef9072804e8f978bb58c2173ac4e9",
+    "url": "/portfolio-website/static/media/ThirdYearDissertationImage.d92ef907.png"
+  },
+  {
+    "revision": "9e12f1c808aeb251366f6e37e3ab58fa",
+    "url": "/portfolio-website/static/media/TowerDefenseImage.9e12f1c8.png"
+  },
+  {
+    "revision": "7b326b44b95e2c4c6449dac6430bdb41",
+    "url": "/portfolio-website/static/media/ZlatImage.7b326b44.jpeg"
+  }
+]);
