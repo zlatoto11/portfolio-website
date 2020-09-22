9068db615e082822ecc983109001514decdfac91
Zlatomir Kosev <zlatomir_hk@hotmail.com>
2020-09-22 18:19:12 +0100

    Changed cv and formatting

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -96,6 +96,11 @@ small{
 
 .leftside{
   font-size:110%;
+  display: flex;
+  text-align:center;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 .linktoprojects{
   color: #ef233c;
